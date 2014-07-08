7cd76d3078144421ff3ec0cfd58adf739f322a76
renan-ranelli <renan.ranelli@locaweb.com.br>
2014-07-07 22:05:13 -0300

    Complete overhaul of frame hooks

--- emacs.d/lisp/init-frame-hooks.el
+++ emacs.d/lisp/init-frame-hooks.el
@@ -2,33 +2,41 @@
 ;;; Commentary:
 ;;; Code:
 
-;; context: if using terminal:
-;; disables global hl line mode
-(add-hook 'after-make-frame-functions
-          (lambda (given-frame)
-            (if (display-graphic-p given-frame)
-                (global-hl-line-mode 1)
-              (global-hl-line-mode -1))))
+;; -- single hook for whole stuff --
+(add-hook 'after-make-frame-functions 'config-new-frame)
 
-;; changes some faces
-(add-hook 'after-make-frame-functions
-          (lambda (given-frame)
-            (unless (display-graphic-p given-frame)
-              (progn
-                (set-face-attribute 'helm-buffer-directory given-frame :foreground "red")
-                (set-face-attribute 'helm-ff-directory given-frame :foreground "red")
-                (set-face-attribute 'helm-selection given-frame :background "cyan" :foreground "black"))))
+;; -- frame config dispatch --
+(defun config-new-frame (frame)
+  "Configure FRAME with specific settings for terminal or x."
+  (if (display-graphic-p frame)
+      (config-x-frame frame)
+    (config-terminal-frame frame)))
 
-;; context: if using X
-(add-hook 'after-make-frame-functions 'toggle-transparency))
+;; -- Terminal frame --
+(defun config-terminal-frame (frame)
+  "Configure terminal FRAME."
+  (set-terminal-faces frame)
+  (global-hl-line-mode -1))
 
-(defun toggle-transparency (&optional given-frame)
-  "Toggle frame transparency."
+(defun set-terminal-faces (frame)
+  "Set specific faces for a terminal FRAME."
+  (set-face-attribute 'helm-buffer-directory frame :foreground "red")
+  (set-face-attribute 'helm-ff-directory frame :foreground "red")
+  (set-face-attribute 'helm-selection frame :background "cyan" :foreground "black"))
+
+;; -- X frame --
+(defun config-x-frame (frame)
+  "Configure x FRAME."
+  (toggle-transparency frame)
+  (global-hl-line-mode 1))
+
+(defun toggle-transparency (&optional frame)
+  "Toggle frame transparency for FRAME.  Use selected frame if frame not given."
   (interactive)
   (let* ((max-transp '(100 100))
-        (min-transp '(90 90))
-        (frame (if given-frame given-frame (selected-frame)))
-        (transp (frame-parameter frame 'alpha)))
+         (min-transp '(90 90))
+         (frame (if frame frame (selected-frame)))
+         (transp (frame-parameter frame 'alpha)))
     (if (equal transp min-transp)
         (set-frame-parameter frame 'alpha max-transp)
       (set-frame-parameter frame 'alpha min-transp))))
