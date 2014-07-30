17580f54f5562b399f4ed719d4f05c3fa8cc45a0
renan-ranelli <renan.ranelli@locaweb.com.br>
2014-07-30 14:09:11 -0300

    Add keybindings for yas

--- lisp/init-yas.el
+++ lisp/init-yas.el
@@ -18,5 +18,9 @@
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
 (global-set-key (kbd "M-y") 'yas-expand)
 
+(define-key yas-minor-mode-map (kbd "C-c y n") 'yas-new-snippet)
+(define-key yas-minor-mode-map (kbd "C-c y i") 'yas-insert-snippet)
+(define-key yas-minor-mode-map (kbd "C-c y f") 'yas-visit-snippet-file)
+
 (provide 'init-yas)
 ;;; init-yas.el ends here
