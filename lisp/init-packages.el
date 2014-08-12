cdf388d8dcbb1fb8aceb25d7c54e321457b96afa
renan-ranelli <renan.ranelli@locaweb.com.br>
2014-08-12 18:17:48 -0300

    Remove unnecessary package sources

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -5,12 +5,6 @@
 (package-initialize)
 
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (when (not package-archive-contents)
