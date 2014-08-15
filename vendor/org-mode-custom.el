5a4b7351fb9f1ef8e4ad030d6f036550d6641aaf
renan-ranelli <renan.ranelli@locaweb.com.br>
2014-08-15 20:05:57 -0300

    Fix ido mode

--- vendor/org-mode-custom.el
+++ vendor/org-mode-custom.el
@@ -160,13 +160,13 @@
 
                                         ; Use IDO for both buffer and file completion and ido-everywhere to t
 (setq org-completion-use-ido t)
-(setq ido-everywhere t)
 (setq ido-max-directory-size 100000)
-(ido-mode (quote both))
-                                        ; Use the current window when visiting files and buffers with ido
-(setq ido-default-file-method 'selected-window)
-(setq ido-default-buffer-method 'selected-window)
-                                        ; Use the current window for indirect buffer display
+;; (setq ido-everywhere t)
+;; (ido-mode (quote both))
+;;                                         Use the current window when visiting files and buffers with ido
+;;(setq ido-default-file-method 'selected-window)
+;;(setq ido-default-buffer-method 'selected-window)
+;;                                         ; Use the current window for indirect buffer display
 (setq org-indirect-buffer-display 'current-window)
 
 ;;;; Refile settings
