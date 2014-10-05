b819cd2c762e67ac67e759221c2197826aad89ab
renan-ranelli <renanranelli@gmail.com>
2014-10-05 15:08:21 -0300

    Chang magit-status binding

--- lisp/init-project-utils.el
+++ lisp/init-project-utils.el
@@ -82,7 +82,7 @@
     (define-key map "n" 'rr-show-file-name)
 
     ;; git
-    (define-key map "m" 'magit-status)
+    (define-key map "g" 'magit-status)
     (define-key map "b" 'magit-branch-manager)
     (define-key map "l" 'magit-log)
     (define-key map "i" 'git-timemachine)
@@ -92,7 +92,7 @@
     (define-key map "a" 'ag-project-regexp)
 
     ;; neotree
-    (define-key map "g" 'neotree-git-project)
+    (define-key map "r" 'neotree-git-project)
     (define-key map "t" 'neotree-toggle)
     (define-key map "f" 'neotree-find)
     map))
