59353445b1010b8f66b384afdfc11568d85c9839
renan-ranelli <renan.ranelli@locaweb.com.br>
2014-10-15 21:28:58 -0300

    More mail configuration.
    Now locaweb's mail is well behaved

--- lisp/init-mail.el
+++ lisp/init-mail.el
@@ -4,17 +4,33 @@
 (autoload 'wl "wl" "Wanderlust" t)
 (require 'wl)
 
+;; I should check something like `http://www.emacswiki.org/emacs/hgw-init-wl.el'
+
+;; -- preferences --
+
 ;; In order to save your passwords, run
 ;; `elmo-passwd-alist-save' interactively
-
 (setq
  ;; do not limit summary width
  wl-summary-width 180
  ;; dont know what that is
  wl-message-buffer-prefetch-depth 0
  ;; do not split large attachments in many messages
- mime-edit-sbplit-message nil)
+ mime-edit-sbplit-message nil
+ ;; do not remember mark direction
+ wl-summary-move-direction-toggle nil
 
+ wl-summary-auto-refile-skip-marks '("!" "$")
+
+ ;; Set header fields to show
+ wl-message-ignored-field-list '("^.*:")
+ wl-message-visible-field-list
+ '("^\\(To\\|Cc\\):"
+   "^Subject:"
+   "^\\(From\\|Reply-To\\):"
+   "^\\(Posted\\|Date\\):"))
+
+;; -- elmo configuration --
 (setq elmo-imap4-default-server "imap.gmail.com"
       elmo-imap4-default-user "renanranelli@gmail.com"
       elmo-imap4-default-authenticate-type 'clear
@@ -27,8 +43,6 @@
 ;; Accounts
 (setq
  wl-template-default-name "locaweb"
-
- wl-default-spec "%INBOX: "
 
  wl-draft-config-matchone t
  wl-draft-reply-buffer-style 'full
@@ -66,15 +80,6 @@
 			 ((string-match "." wl-draft-parent-folder)
 			  (template . "gmail"))))
 
-;; Set header fields to show
-(setq
- wl-message-ignored-field-list '("^.*:")
- wl-message-visible-field-list
- '("^\\(To\\|Cc\\):"
-   "^Subject:"
-   "^\\(From\\|Reply-To\\):"
-   "^\\(Posted\\|Date\\):"))
-
 ;; sorting backwards
 (defun wl-summary-overview-entity-compare-by-rdate (x y)
   (not (wl-summary-overview-entity-compare-by-date x y)))
@@ -96,6 +101,18 @@
     (rr-strip-whitespace)
     (read-only-mode 1)))
 
+;; -- auto refile configuration --
+(let ((junk-folder "%Junk:\"renan.ranelli\"/clear@outlook.locaweb.com.br:993!")
+      (prb-folder "%PRBs:\"renan.ranelli\"/clear@outlook.locaweb.com.br:993!"))
+  (setq wl-refile-rule-alist
+	;; (field (regexp . target) ...)
+	`(("Subject" ("Confirmação de contratação de produtos" . ,junk-folder))
+	  ("Subject" ("\\[PRB[0-9]+\\] - \\[HOSP\\]" . ,prb-folder))
+	  ("Subject" ("\\[PRB[0-9]+\\] - \\[" . ,junk-folder))
+	  ("From" ("info@locaweb.com.br" . ,junk-folder))
+	  ("From" ("continuous.integration@locaweb.com.br" . ,junk-folder))
+	  ("Subject" ("\\[Ger\\. Mudanças\\] - Changes para Implantação" . ,prb-folder)))))
+
 ;; -- bindings --
 (define-key global-map (kbd "<f11>") 'wl)
 
@@ -107,9 +124,11 @@
 	    			       (wl-summary-toggle-disp-msg)))
 	    (local-set-key (kbd "n") 'mime-preview-next-line-entity)
 	    (local-set-key (kbd "p") 'mime-preview-previous-line-entity)
+	    (local-set-key (kbd "f") 'wl-fill-cleanup-fuckedup-message)
 	    (local-set-key (kbd "C-c m f") 'wl-fill-cleanup-fuckedup-message)))
 
 (define-key wl-summary-mode-map (kbd "RET") 'wl-summary-jump-to-current-message)
+(define-key wl-summary-mode-map (kbd "O") 'wl-summary-refile-prev-destination)
 
 (expose-bindings wl-summary-mode-map bindings-to-expose)
 (expose-bindings wl-template-mode-map bindings-to-expose)
