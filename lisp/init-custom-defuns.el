5f667ee2b95fbc0af87f28b61ebe0e5f50165433
renan-ranelli <renanranelli@gmail.com>
2014-10-19 23:29:00 -0200

    Add nice advices for C-w and M-w

--- lisp/init-custom-defuns.el
+++ lisp/init-custom-defuns.el
@@ -93,6 +93,23 @@
 	    (re-search-forward "^\\(.*\\)\n\\(\\(.*\n\\)*\\)\\1\n" end t))
 	(replace-match "\\1\n\\2")))))
 
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single
+line instead."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single
+line instead."
+  (interactive
+   (if mark-active
+       (list (region-beginning) (region-end))
+     (message "Copied line")
+     (list (line-beginning-position) (line-beginning-position 2)))))
+
 (defun uniquify-all-lines-buffer ()
   "Delete duplicate lines in buffer and keep first occurrence."
   (interactive "*")
