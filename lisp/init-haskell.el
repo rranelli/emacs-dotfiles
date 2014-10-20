40665402ac173ceb27fcc3dc92a711d11ac28005
renan-ranelli <renanranelli@gmail.com>
2014-10-19 23:25:19 -0200

    Now I can add custom symbols to haskell's font-lock

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -3,11 +3,29 @@
 ;;; Code:
 (setq haskell-font-lock-symbols t)
 
-(remove-hook 'haskell-mode-hook 'pretty-symbols-mode)
-
-(add-hook 'haskell-mode-hook 'turn-on-haskell-font-lock)
 (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 (add-hook 'haskell-mode-hook 'inf-haskell-mode)
+
+;; -- haskell font lock --
+(add-hook 'haskell-mode-hook 'turn-on-haskell-font-lock)
+(remove-hook 'haskell-mode-hook 'pretty-symbols-mode)
+
+(defvar rr-haskell-font-lock-extra-symbols
+  '(("<alpha>" . #X03B1)
+    ("<beta>" . #X03B2)
+    ("<gamma>" . #X03B3)
+    ("<delta>" . #X03B4)
+    ("''" . #X2033)
+    ("'" . #X2032)
+    (".." . #X2026)))
+
+(eval-after-load 'haskell-font-lock
+  '(progn
+     (mapcar
+      (lambda (entry) (add-to-list 'haskell-font-lock-symbols-alist entry))
+      rr-haskell-font-lock-extra-symbols)
+     (setq haskell-font-lock-keywords
+	   (haskell-font-lock-keywords-create nil))))
 
 (provide 'init-haskell)
 ;;; init.el ends here
