cc309d85b864dc62a1cafb59fbbd8a897ba970fa
renan-ranelli <renanranelli@gmail.com>
2014-10-28 02:10:06 -0200

    Add more usefull functions for maven testing

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -7,17 +7,54 @@
 (javadoc-add-artifacts [com.nullprogram native-guide "0.2"]
                        [org.apache.commons commons-math3 "3.0"])
 
+(require 's)
+(require 'ffip)
+
 (defun rr-maven-test-all ()
   "Run maven test task."
   (interactive)
-  (let ((mvn-root (ffip-project-root)))
-    (compile (format
-	      "cd %s && mvn test; cat %s/target/surefire-reports/*.txt"
-	      mvn-root
-	      mvn-root))))
+  (compile (s-concat
+	    (rr-maven-format-test-task)
+	    (rr-maven-format-result-cat))))
+
+(defun rr-maven-test-file ()
+  "Run maven test task for current file."
+  (interactive)
+  (compile (s-concat
+	    (rr-maven-format-test-task)
+	    (rr-maven-java-class-from-buffer)
+	    (rr-maven-format-result-cat))))
+
+(defun rr-maven-test-method ()
+  "Run maven test task for current method"
+  (interactive)
+  (compile (s-concat
+	    (rr-maven-format-test-task)
+	    (rr-maven-java-class-from-buffer)
+	    (rr-maven-get-prev-test-method)
+	    (rr-maven-format-result-cat))))
+
+(defun rr-maven-format-test-task ()
+  (format "cd %s && mvn test" (ffip-project-root)))
+
+(defun rr-maven-format-result-cat ()
+  (format ";cat %s/target/surefire-reports/*.txt" (ffip-project-root)))
+
+(defun rr-maven-java-class-from-buffer ()
+  (let* ((class-file (file-name-base (buffer-file-name)))
+	 (class-name (s-replace ".java" "" class-file)))
+    (format " -Dtest=%s" class-name)))
+
+(defun rr-maven-get-prev-test-method ()
+  (save-excursion
+    (re-search-backward "void \\(test[a-zA-Z]+\\) *() *{")
+    (s-concat "#" (match-string 1))))
 
 (define-bindings java-mode-map
   '(("C-c , a" . rr-maven-test-all)
+    ("C-c , v" . rr-maven-test-file)
+    ("C-c , s" . rr-maven-test-method)
+    ("C-c , r" . recompile)
     ("C-c j d" . javadoc-lookup)
     ("C-c j s" . sort-java-imports)
     ("C-c j i" . add-java-import)))
