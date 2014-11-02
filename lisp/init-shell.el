89f61fc5b4e0a6dbba7610372f8d6125e9a8997f
renan-ranelli <renanranelli@gmail.com>
2014-11-02 18:56:18 -0200

    Refactor init-shell

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -1,7 +1,6 @@
 ;;; package -- Summary
 ;;; Commentary:
 ;;; Code:
-
 (autoload 'bash-completion-dynamic-complete
   "bash-completion"
   "BASH completion hook")
@@ -17,60 +16,61 @@
       (kill-buffer "*Completions*")))
   output)
 
-(defadvice shell (after do-not-query-shell-exit
-			first (&optional buffer)
-			activate)
-  "Do not query exit confirmation for shell process buffer."
-  (interactive)
-  (let* ((shell-processes (remove-if-not
-			   (lambda (process) (string-match-p "shell" (process-name process)))
-			   (process-list))))
-    (dolist (p shell-processes)
-      (set-process-query-on-exit-flag p nil))))
+(defmacro do-not-query-process-kill (function-name process-name)
+  "Do not query process kill for FUNCTION-NAME that spawns process
+PROCESS-NAME."
+  `(defadvice ,function-name (after do-not-query-shell-exit
+				    first (&optional buffer)
+				    activate)
+     (interactive)
+     "Do not query exit confirmation for shell process buffer."
+     (let* ((processes (remove-if-not
+			(lambda (process) (string-match-p ,process-name (process-name process)))
+			(process-list))))
+       (dolist (p processes)
+	 (set-process-query-on-exit-flag p nil)))))
 
-(defadvice term (after do-not-query-term-exit
-		       first (&optional buffer)
-		       activate)
-  "Do not query exit confirmation for shell process buffer."
-  (interactive)
-  (let* ((shell-processes (remove-if-not
-			   (lambda (process) (string-match-p "terminal" (process-name process)))
-			   (process-list))))
-    (dolist (p shell-processes)
-      (set-process-query-on-exit-flag p nil))))
+(do-not-query-process-kill shell "shell")
+(do-not-query-process-kill term "terminal")
 
 (defun new-shell (arg)
-  "Create shell with given name. If ARG is present, open a new shell regardless ."
+  "Create shell with given name. If ARG is present, open a new shell
+regardless."
   (interactive "P")
-  (let* ((project-root (ffip-project-root))
-	 (in-project-p (stringp project-root))
-	 (dir-name (when project-root
-		     (file-name-base (directory-file-name project-root))))
-
-	 (project-name (format "<%s>" (if in-project-p
-					  dir-name
-					"out-of-project")))
-	 (custom-name (if arg
-			  (format " [%s]" (read-string "Shell name: "))
+  (let* ((custom-name (if arg
+			  (format "[%s]" (read-string "Shell name: "))
 			""))
-	 (shell-name (format "shell: %s%s" project-name custom-name))
-
+	 (shell-name (format "shell: %s %s" (rr-shell-project-name) custom-name))
 	 (shell-exists-p (bufferp (get-buffer shell-name))))
 
     (shell shell-name)
+
     (when (and
-	   in-project-p
+	   (rr-in-project)
 	   (not shell-exists-p))
-      (insert (format
-	       "cd %s # Press [RET] to go to root"
-	       project-root)))))
+      (goto-char (point-max))
+      (insert (format "cd %s # [Enter] cds to root" (rr-shell-wd))))))
+
+(defun rr-shell-project-name ()
+  (file-name-base (directory-file-name (rr-shell-wd))))
+
+(defun rr-shell-wd ()
+  (if (rr-in-project)
+      (ffip-project-root)
+    default-directory))
+
+(defun rr-in-project ()
+  (stringp (ffip-project-root)))
 
 ;; hooks
 (add-hook 'comint-preoutput-filter-functions 'kill-completion-window-buffer)
 
 ;; -- keybindings --
 (global-set-key (kbd "C-x RET") 'new-shell)
-(global-set-key (kbd "C-x C-M-M") (lambda () (interactive) (term "/bin/bash")))
+(global-set-key (kbd "C-x C-M-M") #'(lambda ()
+				      (interactive)
+				      (term "/bin/bash")
+				      (rename-buffer (format "term: %s" (rr-shell-project-name)))))
 
 (expose-bindings shell-mode-map bindings-to-expose)
 (add-hook 'term-mode-hook
