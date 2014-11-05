b0cf7f26b83fe0b4ada0a8d8a31856141c3d2b89
renan-ranelli <renan.ranelli@locaweb.com.br>
2014-11-05 16:20:15 -0200

    Better ruby-mode bindings setup

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -106,20 +106,19 @@
   (other-window 1))
 
 ;; -- keybindings --
-(define-key rspec-mode-keymap (kbd "y") 'rspec-spec-or-target-other-window-no-change-window)
-(define-key rspec-mode-keymap (kbd "u") 'rspec-find-spec-or-target-other-window)
-(define-key rspec-mode-keymap (kbd "e") 'rspec-find-spec-or-target-find-example-other-window)
-(define-key rspec-mode-keymap (kbd "w") 'rspec-toggle-spec-and-target-find-example)
+(dolist (map '(rspec-mode-keymap rspec-verifiable-mode-keymap))
+  (define-bindings map
+    '(("y" . rspec-spec-or-target-other-window-no-change-window)
+      ("u" . rspec-find-spec-or-target-other-window)
+      ("e" . rspec-find-spec-or-target-find-example-other-window)
+      ("w" . rspec-toggle-spec-and-target-find-example))))
 
-(defvar ruby-mode-custom-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "b") 'ruby-send-buffer)
-    (define-key map (kbd "r") 'ruby-send-region)
-    (define-key map (kbd "v") 'ruby-refactor-extract-local-variable)
-    (define-key map (kbd "m") 'ruby-refactor-extract-to-method)
-    (define-key map (kbd "l") 'ruby-refactor-extract-to-let)
-    map))
-(define-key ruby-mode-map (kbd "C-c r") ruby-mode-custom-map)
+(define-bindings ruby-mode-map
+  '(("C-c r b" . ruby-send-buffer)
+    ("C-c r r" . ruby-send-region)
+    ("C-c r v" . ruby-refactor-extract-local-variable)
+    ("C-c r m" . ruby-refactor-extract-to-method)
+    ("C-c r l" . ruby-refactor-extract-to-let)))
 
 (provide 'init-ruby)
 ;;; init-ruby.el ends here
