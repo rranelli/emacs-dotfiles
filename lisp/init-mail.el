7f8c5daff279b86853fc0191eb534d270b0f50c0
renan-ranelli <renan.ranelli@locaweb.com.br>
2014-11-11 14:15:24 -0200

    GerMudancas goes to junk folder

--- lisp/init-mail.el
+++ lisp/init-mail.el
@@ -113,7 +113,7 @@
 	`(("Subject" ("Confirmação de contratação de produtos" . ,junk-folder))
 	  ("Subject" ("\\[PRB[0-9]+\\] - \\[HOSP\\]" . ,prb-folder))
 	  ("Subject" ("\\[PRB[0-9]+\\] - " . ,junk-folder))
-	  ("Subject" ("\\[Ger\\. Mudanças\\]" . ,prb-folder))
+	  ("Subject" ("\\[Ger\\. Mudanças\\]" . ,junk-folder))
 	  ("From" ("info@locaweb.com.br" . ,junk-folder))
 	  ("From" ("continuous.integration@locaweb.com.br" . ,junk-folder))
 	  ("From" ("billing@softaculous.com" . ,junk-folder))
