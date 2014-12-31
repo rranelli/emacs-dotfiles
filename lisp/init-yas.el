c15aff3783fc24d05a7be3c289700f7f5d6b8670
renan-ranelli <renanranelli@gmail.com>
2014-12-31 02:14:10 -0200

    Add guard for adding `yasnippet` to `ac-sources`

--- lisp/init-yas.el
+++ lisp/init-yas.el
@@ -2,22 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 (require 'yasnippet)
+(yas-global-mode 1)
 
 (defcustom rr-yas-snippet-dirs
   (expand-file-name "snippets" user-emacs-directory)
   "Directory to load yasnippet's snippet files."
   :group 'init-yas)
-
-(yas-global-mode 1)
-(add-hook 'prog-mode-hook
-          '(lambda () (yas-minor-mode)))
-
 (setq yas-snippet-dirs rr-yas-snippet-dirs)
+
 (setq yas-prompt-functions (delete 'yas-x-prompt yas-prompt-functions))
 
 ;; add to autocomplete sources
-(if (boundp 'ac-sources)
-    (add-to-list 'ac-sources ac-source-yasnippet))
+(when (and (boundp 'ac-sources)
+	   (file-exists-p yas-snippet-dirs))
+  (add-to-list 'ac-sources ac-source-yasnippet))
 
 ;; -- keybindings --
 (define-key yas-minor-mode-map (kbd "TAB") nil)
@@ -28,6 +26,10 @@
 (define-key yas-minor-mode-map (kbd "C-c y n") 'yas-new-snippet)
 (define-key yas-minor-mode-map (kbd "C-c y f") 'yas-visit-snippet-file)
 (define-key yas-minor-mode-map (kbd "C-c y i") 'yas-insert-snippet)
+
+;; -- hooks --
+(add-hook 'prog-mode-hook
+          '(lambda () (yas-minor-mode)))
 
 (provide 'init-yas)
 ;;; init-yas.el ends here
