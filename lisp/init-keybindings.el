dbddcaabfbe0674a56afb1475eff44aa2713f814
renan-ranelli <renanranelli@gmail.com>
2015-01-27 01:18:25 -0200

    Define keybinding for wrapper replace

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -91,6 +91,8 @@
 (global-set-key (kbd "C-:") 'ace-jump-char-mode)
 
 (global-set-key (kbd "M-j") 'better-registers-jump-to-register)
+(define-key better-registers-map (kbd "C-r (") 'rr-replace-wrapper-around-point)
+
 
 ;; window and buffer manipulation
 (global-set-key (kbd "C->") 'maximize-window-vertically)
