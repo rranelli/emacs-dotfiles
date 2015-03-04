19f16be6c18deb93b5a892c3e05fad9513578931
renan-ranelli <renanranelli@gmail.com>
2015-03-04 00:32:44 -0300

    Smartparens config for elixir mode

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -68,5 +68,15 @@
 (sp-with-modes '(html-mode sgml-mode)
   (sp-local-pair "<" ">"))
 
+(sp-with-modes '(elixir-mode)
+  (sp-local-pair "fn" "end"
+		 :when '(("SPC" "RET"))
+		 :post-handlers '(sp-ruby-def-post-handler)
+		 :actions '(insert navigate))
+  (sp-local-pair "do" "end"
+		 :when '(("SPC" "RET"))
+		 :post-handlers '(sp-ruby-def-post-handler)
+		 :actions '(insert navigate)))
+
 (provide 'init-smartparens)
 ;;; init-smartparens.el ends here
