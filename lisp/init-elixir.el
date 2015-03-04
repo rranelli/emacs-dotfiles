488110af95b33ee510c6fc5f71b160ac0857aa96
renan-ranelli <renanranelli@gmail.com>
2015-03-04 02:16:43 -0300

    Friendlier bindings for test running inside alchemist

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -4,7 +4,14 @@
 (require 'elixir-mode)
 (require 'alchemist)
 
+;; -- hooks --
 (add-hook 'elixir-mode-hook 'alchemist-mode)
+
+(define-bindings alchemist-mode-map
+  '(("C-c , y" . alchemist-project-open-tests-for-current-file)
+    ("C-c , a" . alchemist-mix-test)
+    ("C-c , s" . alchemist-mix-test-at-point)
+    ("C-c , v" . alchemist-mix-test-this-buffer)))
 
 (provide 'init-elixir)
 ;;; init-elixir.el ends here
