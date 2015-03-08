b5f48895eff26faebd08420f30c5c51cc9229498
renan-ranelli <renanranelli@gmail.com>
2015-03-08 02:26:06 -0300

    Add my own alchemist test and implementation toggle functions

--- lisp/init-elixir.el
+++ lisp/init-elixir.el
@@ -19,6 +19,36 @@
     (backward-char)
     (fill-region (point) (mark))))
 
+(defun rr/alchemist-toggle-file-and-tests-other-window ()
+  (interactive)
+  (if (rr/alchemist--is-test-file-p)
+      (rr/alchemist--project-open-file-for-current-tests 'find-file-other-window)
+    (rr/alchemist--project-open-tests-for-current-file 'find-file-other-window)))
+
+(defun rr/alchemist-toggle-file-and-tests ()
+  (interactive)
+  (if (rr/alchemist--is-test-file-p)
+      (rr/alchemist--project-open-file-for-current-tests 'find-file)
+    (rr/alchemist--project-open-tests-for-current-file 'find-file)))
+
+(defun rr/alchemist--is-test-file-p ()
+  (string-match "_test\.exs$" (buffer-file-name)))
+
+(defun rr/alchemist--project-open-file-for-current-tests (toggler)
+  (let* ((filename (file-relative-name (buffer-file-name) (alchemist-project-root)))
+         (filename (replace-regexp-in-string "^test/" "lib/" filename))
+         (filename (replace-regexp-in-string "_test\.exs$" "\.ex" filename))
+         (filename (format "%s/%s" (alchemist-project-root) filename)))
+    (funcall toggler filename)))
+
+(defun rr/alchemist--project-open-tests-for-current-file (toggler)
+  (interactive)
+  (let* ((filename (file-relative-name (buffer-file-name) (alchemist-project-root)))
+         (filename (replace-regexp-in-string "^lib/" "test/" filename))
+         (filename (replace-regexp-in-string "\.ex$" "_test\.exs" filename))
+         (filename (format "%s/%s" (alchemist-project-root) filename)))
+    (funcall toggler filename)))
+
 ;; pretty symbols
 (setq pretty-symbol-patterns
       (append pretty-symbol-patterns
@@ -29,7 +59,8 @@
 		)))
 
 (define-bindings alchemist-mode-map
-  '(("C-c , y" . alchemist-project-open-tests-for-current-file)
+  '(("C-c , y" . rr/alchemist-toggle-file-and-tests-other-window)
+    ("C-c , t" . rr/alchemist-toggle-file-and-tests)
     ("C-c , a" . alchemist-mix-test)
     ("C-c , s" . alchemist-mix-test-at-point)
     ("C-c , v" . alchemist-mix-test-this-buffer)))
