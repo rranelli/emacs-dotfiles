4a129375adabfd96db9740f07150702870e9265d
renan-ranelli <renanranelli@gmail.com>
2015-03-17 19:12:12 -0300

    C-h should **always** mean backward-delete-char -- Always

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -10,7 +10,8 @@
 
 (define-bindings company-active-map
   `(("C-n" . company-select-next)
-    ("C-p" . company-select-previous)))
+    ("C-p" . company-select-previous)
+    ("C-h" . backward-delete-char)))
 
 (provide 'init-company)
 ;;; init-company.el ends here
