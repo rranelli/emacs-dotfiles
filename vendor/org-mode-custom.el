d228613a9e488a7ec601b08bb6c7f6154a4c8321
renan-ranelli <renanranelli@gmail.com>
2015-06-04 22:56:49 -0300

    Remove appt configuration from org-mode-custom.el

--- vendor/org-mode-custom.el
+++ vendor/org-mode-custom.el
@@ -811,24 +811,6 @@ Skip project and sub-project tasks, habits, and loose non-project tasks."
 
 (setq org-export-allow-BIND t)
 
-                                        ; Erase all reminders and rebuilt reminders for today from the agenda
-(defun bh/org-agenda-to-appt ()
-  (interactive)
-  (setq appt-time-msg-list nil)
-  (org-agenda-to-appt))
-
-                                        ; Rebuild the reminders everytime the agenda is displayed
-(add-hook 'org-finalize-agenda-hook 'bh/org-agenda-to-appt 'append)
-
-                                        ; This is at the end of my .emacs - so appointments are set up when Emacs starts
-(bh/org-agenda-to-appt)
-
-                                        ; Activate appointments so we get notifications
-(appt-activate t)
-
-                                        ; If we leave Emacs running overnight - reset the appointments one minute after midnight
-(run-at-time "24:01" nil 'bh/org-agenda-to-appt)
-
 ;; Skeletons
 ;;
 ;; sblk - Generic block #+begin_FOO .. #+end_FOO
