2947cbc39081f79e75d140128f34cc119c94bde1
renan-ranelli <renanranelli@gmail.com>
2015-06-13 19:59:05 -0300

    Now cycling between a preset of highlight faces

--- lisp/init-custom-defuns.el
+++ lisp/init-custom-defuns.el
@@ -109,11 +109,32 @@ narrowed."
   (interactive)
   (set-buffer-file-coding-system 'utf-8 t))
 
+(defun rr/highlight-next-face ()
+  "Gets the next highlight face from list"
+  (interactive)
+  (car (setq rr/highlight-faces
+             (-rotate 1 rr/highlight-faces))))
+
+(defvar rr/highlight-faces
+  '(helm-match-item
+    hi-yellow
+    hi-blue
+    hi-green
+    ido-indicator
+    helm-selection
+    highlight
+    popup-menu-mouse-face
+    hi-pink
+    magit-log-message
+    magit-log-reflog-label-cherry-pick
+    neo-vc-unlocked-changes-face
+    org-agenda-filter-category))
+
 (defun rr/highlight-at-point ()
   "Highlight the sexp at point."
   (interactive)
   (let ((rgxp (regexp-quote (symbol-name (sexp-at-point)))))
-    (highlight-regexp rgxp)))
+    (highlight-regexp rgxp (rr/highlight-next-face))))
 
 (defun rr/unhighlight-at-point ()
   "Unhighlight the sexp at point."
