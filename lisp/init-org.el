487cb005385a2ead8535e3cd7e9a3b0f46eb2bc7
renan-ranelli <renanranelli@gmail.com>
2015-06-18 01:23:18 -0300

    No more annoying as fuck gpg key prompt

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -54,6 +54,29 @@
       (setq org-tags-exclude-from-inheritance (quote ("crypt")))
       (setq org-crypt-key nil)
 
+      (defadvice epg--start (around advice-epg-disable-agent disable)
+        "Make epg--start not able to find a gpg-agent"
+        (let ((agent (getenv "GPG_AGENT_INFO")))
+          (setenv "GPG_AGENT_INFO" nil)
+          ad-do-it
+          (setenv "GPG_AGENT_INFO" agent)))
+
+      (defun epg-enable-agent ()
+        "Make EasyPG use a gpg-agent after having been disabled with epg-disable-agent"
+        (interactive)
+        (ad-disable-advice 'epg--start 'around 'advice-epg-disable-agent)
+        (ad-activate 'epg--start)
+        (message "EasyPG gpg-agent re-enabled"))
+
+      (defun epg-disable-agent ()
+        "Make EasyPG bypass any gpg-agent"
+        (interactive)
+        (ad-enable-advice 'epg--start 'around 'advice-epg-disable-agent)
+        (ad-activate 'epg--start)
+        (message "EasyPG gpg-agent bypassed"))
+
+      (epg-disable-agent)
+
       ;; org notification magic
       (require 'org-notify)
       (org-notify-start 60)
