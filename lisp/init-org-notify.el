18ac0aebdfbe5cc65c40a2c56fb7746271ce85fb
renan-ranelli <renanranelli@gmail.com>
2015-08-16 02:54:12 -0300

    Do not notify tasks in periods greater than 2 hours

--- lisp/init-org-notify.el
+++ lisp/init-org-notify.el
@@ -4,10 +4,11 @@
 
 (require 'org-notify)
 (org-notify-start 60)
+
 (org-notify-add 'default
                 '(:time "10m" :period "2m" :duration 25 :actions -notify/window)
-                '(:time "1h" :period "20m" :duration 25 :actions -notify/window)
-                '(:time "1d" :period "90m" :duration 25 :actions -notify/window)
-                '(:time "2d" :period "3h" :duration 25 :actions -notify/window))
+                '(:time "1h" :period "15m" :duration 25 :actions -notify/window)
+                '(:time "2h" :period "30m" :duration 25 :actions -notify/window))
+
 (provide 'init-org-notify)
 ;;; init-org-notify.el ends here
