271204b46fcbaa96cd352416ac0af80299a95777
renan-ranelli <renanranelli@gmail.com>
2015-08-17 17:22:35 -0300

    Maybe this will fix the freezes ???

--- lisp/init-org-mobile.el
+++ lisp/init-org-mobile.el
@@ -5,26 +5,26 @@
 (setq org-mobile-directory "/scp:renan@cpro26266.publiccloud.com.br:/home/renan/mobileorg/"
       org-mobile-inbox-for-pull (expand-file-name "refile.org" rr/org-files-directory))
 
-(defvar org-mobile-push-timer nil
-  "Timer that `org-mobile-push-timer' used to reschedule itself, or nil.")
+;; (defvar org-mobile-push-timer nil
+;;   "Timer that `org-mobile-push-timer' used to reschedule itself, or nil.")
 
-(defun org-mobile-push-with-delay (secs)
-  (when org-mobile-push-timer
-    (cancel-timer org-mobile-push-timer))
-  (setq org-mobile-push-timer
-        (run-with-idle-timer
-         (* 1 secs) nil 'org-mobile-push)))
+;; (defun org-mobile-push-with-delay (secs)
+;;   (when org-mobile-push-timer
+;;     (cancel-timer org-mobile-push-timer))
+;;   (setq org-mobile-push-timer
+;;         (run-with-idle-timer
+;;          (* 1 secs) nil 'org-mobile-push)))
 
-(add-hook 'after-save-hook
-          (lambda ()
-            (when (eq major-mode 'org-mode)
-              (dolist (file org-user-agenda-files)
-                (if (string= (file-truename (expand-file-name file))
-                             (file-truename (buffer-file-name)))
-                    (org-mobile-push-with-delay 1200))))))
+;; (add-hook 'after-save-hook
+;;           (lambda ()
+;;             (when (eq major-mode 'org-mode)
+;;               (dolist (file org-user-agenda-files)
+;;                 (if (string= (file-truename (expand-file-name file))
+;;                              (file-truename (buffer-file-name)))
+;;                     (org-mobile-push-with-delay 1200))))))
 
-(run-at-time "23:59" 86400 '(lambda () (org-mobile-push-with-delay 120))) ;; refreshes agenda file each day
-(run-at-time "23:58" 86400 'org-mobile-pull) ;; refreshes agenda file each day
+;; (run-at-time "23:59" 86400 '(lambda () (org-mobile-push-with-delay 120))) ;; refreshes agenda file each day
+;; (run-at-time "23:58" 86400 'org-mobile-pull) ;; refreshes agenda file each day
 
 (provide 'init-org-mobile)
 ;;; init-org-mobile.el ends here
