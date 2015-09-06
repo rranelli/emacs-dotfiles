ee72bd1940517ffea3b4674b5dbc90c706f1e4fb
renan-ranelli <renanranelli@gmail.com>
2015-09-06 18:07:50 -0300

    Org mobile push and pull without scp

--- lisp/init-org-mobile.el
+++ lisp/init-org-mobile.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq org-mobile-directory "/scp:renan@cpro26266.publiccloud.com.br:/home/renan/mobileorg/"
+(setq org-mobile-directory (expand-file-name "mobileorg/" rr/org-files-directory)
       org-mobile-inbox-for-pull (expand-file-name "refile.org" rr/org-files-directory))
 
 (defvar org-mobile-push-timer nil
@@ -15,16 +15,17 @@
         (run-with-idle-timer
          (* 1 secs) nil 'org-mobile-push)))
 
-;; (add-hook 'after-save-hook
-;;           (lambda ()
-;;             (when (eq major-mode 'org-mode)
-;;               (dolist (file org-user-agenda-files)
-;;                 (if (string= (file-truename (expand-file-name file))
-;;                              (file-truename (buffer-file-name)))
-;;                     (org-mobile-push-with-delay 1200))))))
+(add-hook 'after-save-hook
+          (lambda ()
+            (when (eq major-mode 'org-mode)
+              (dolist (file org-user-agenda-files)
+                (if (string= (file-truename (expand-file-name file))
+                             (file-truename (buffer-file-name)))
+                    (org-mobile-push-with-delay 1200))))))
 
-(run-at-time "23:59" 86400 '(lambda () (org-mobile-push-with-delay 120))) ;; refreshes agenda file each day
-;; (run-at-time "23:58" 86400 'org-mobile-pull) ;; refreshes agenda file each day
+ ;; refreshes agenda file each day
+(run-at-time "23:59" 86400 '(lambda () (org-mobile-push-with-delay 120)))
+(run-at-time "23:58" 86400 'org-mobile-pull)
 
 (provide 'init-org-mobile)
 ;;; init-org-mobile.el ends here
