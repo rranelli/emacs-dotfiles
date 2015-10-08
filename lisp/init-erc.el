5d7e861518e43cc03df22bbb0e1cb87c41df4c4b
renan-ranelli <renanranelli@gmail.com>
2015-10-08 16:10:44 -0300

    Fix automatic dictionary spelling

--- lisp/init-erc.el
+++ lisp/init-erc.el
@@ -33,23 +33,62 @@
 (erc-netsplit-mode t)
 (erc-timestamp-mode t)
 (erc-button-mode (- 1))
+(erc-spelling-mode t)
+(erc-log-mode)
 
-(setq erc-track-exclude-types '("MODE" "AWAY" "JOIN" "PART")
-      erc-track-use-faces t
-      erc-hide-list '("JOIN" "PART" "QUIT" "AWAY")
-      erc-max-buffer-size 20000
-      erc-auto-query 'bury
-      erc-query-display 'buffer
-      erc-query-display 'buffer
-      erc-log-insert-log-on-open t
-      erc-log-channels t
-      erc-log-channels-directory "~/.irclogs/"
-      erc-save-buffer-on-part t
-      erc-kill-buffer-on-part t)
+(setq erc-spelling-dictionaries '(("locaweb.irc.slack.com:6667" "brasileiro")))
+
+(setq
+ erc-track-exclude-types '("MODE" "AWAY" "JOIN" "PART")
+ erc-track-use-faces t
+ erc-hide-list '("JOIN" "PART" "QUIT" "NICK" "AWAY" "MODE")
+ erc-max-buffer-size 20000
+ erc-auto-query 'bury
+ erc-query-display 'buffer
+ erc-query-display 'buffer
+ erc-log-channels t
+ erc-log-channels-directory "~/.irclogs/"
+ erc-log-insert-log-on-open nil
+ erc-log-write-after-insert t
+ erc-log-write-after-send t
+ erc-kill-buffer-on-part t
+ erc-keywords '("deploy")
+ erc-input-line-position -2
+ erc-prompt "⟹")
 
 (setq erc-autojoin-channels-alist
       '(("freenode" "#haskell" "haskell-emacs" "#elixir-lang" "#emacs-elixir" "#emacs" "#ruby-lang")))
 
+;;; view logs
+(require 'erc-view-log)
+
+(defun erc-get-color-for-nick (nick)
+  "Gets a color for NICK. If NICK is specified in erc-nick-color-alist, use it, else hash the nick and get a color from that"
+  (or (cdr (assoc nick erc-nick-color-alist))
+      (nth
+       (mod (string-to-number
+	     (substring (md5 nick) 0 6) 16)
+	    (length erc-colors-list))
+       erc-colors-list)))
+
+(defun erc-get-face-for-nick (nick)
+  "Returns the face for the given nick."
+  `((:foreground ,(erc-get-color-for-nick nick))
+    (:weight bold)))
+
+(setq erc-view-log-nickname-face-function 'erc-get-face-for-nick)
+(setq erc-view-log-my-nickname-match `("milhouse" "milhouse`")) ;set this one in your .priv_emacs with your other nicks if needed
+(add-to-list 'auto-coding-alist '("\\.irclogs/.*\\.txt" . utf-8))
+(add-to-list 'auto-mode-alist '("\\.irclogs/.*\\.txt" . erc-view-log-mode))
+
+(defun rr/erc-browse-log ()
+  (interactive)
+  (find-file (erc-current-logfile))
+  (end-of-buffer))
+
+;;
+;;; custom functions
+;;
 (defun rr/irc-freenode ()
   "Connect to freenode IRC."
   (interactive)
@@ -62,6 +101,7 @@
 (defun rr/irc-locaweb-slack ()
   "Connect to locaweb's Slack via IRC."
   (interactive)
+  (add-to-list 'erc-networks-alist '(locaweb "locaweb.irc.slack.com"))
   (erc-tls :server "locaweb.irc.slack.com"
            :port 6667
            :nick "milhouse"
@@ -70,6 +110,7 @@
 
 (defun rr/join-irc ()
   "Connect to all irc servers"
+  (interactive)
   (rr/irc-freenode)
   (rr/irc-locaweb-slack))
 
@@ -79,13 +120,20 @@
   (setq erc-modified-channels-alist nil)
   (erc-modified-channels-display))
 
+;; this is a bugfix
+;; source: http://www.emacswiki.org/emacs/ErcLogging
+(defun erc-log-all-but-server-buffers (buffer)
+  (with-current-buffer buffer
+    (not (erc-server-buffer-p))))
+
 ;;
 ;;; keybindings
 ;;
 (global-set-key (kbd "C-c C-u") 'rr/clear-erc-unseen)
 
-(define-key erc-mode-map (kbd "C-x C-s") 'ignore)
+(define-key erc-mode-map (kbd "C-x C-s") 'erc-save-buffer-in-logs)
 (define-key erc-mode-map (kbd "C-c C-u") 'rr/clear-erc-unseen)
+(define-key erc-mode-map (kbd "C-c C-l") 'rr/erc-browse-log)
 (define-key erc-mode-map (kbd "C-l") '(lambda () (interactive) (erc-cmd-CLEAR)))
 
 (provide 'init-erc)
