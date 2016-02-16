2315e79e045b4fd55efe0c84bffe96f068ae4e9a
renan-ranelli <renanranelli@gmail.com>
2016-02-16 13:11:55 -0200

    No more sensitive backspace @ helm

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -12,14 +12,6 @@
       helm-ls-git-show-abs-or-relative 'relative
       helm-buffer-max-length 45)
 
-;; helpers for more familiar helm find-file navigation
-(defun helm-find-files-sensitive-backspace ()
-  "Deletes whole directory in helm find files mode on backspace."
-  (interactive)
-  (if (char-equal ?/ (char-before))
-      (helm-find-files-up-one-level 1)
-    (backward-delete-char 1)))
-
 ;; Jeez, no fuzzy finding please!
 (setq helm-M-x-fuzzy-match nil)
 (setq helm-locate-fuzzy-match nil)
@@ -33,11 +25,6 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (global-set-key (kbd "C-c h") 'helm-command-prefix)
-
-;; helm better navigation
-(define-key helm-find-files-map (kbd "<backspace>") 'helm-find-files-sensitive-backspace)
-(define-key helm-find-files-map (kbd "<DEL>") 'helm-find-files-sensitive-backspace)
-(define-key helm-find-files-map (kbd "C-h") 'helm-find-files-sensitive-backspace)
 
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 
