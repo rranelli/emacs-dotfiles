b09ff28eda88059cd619a005822a8655e78fa75f
renan-ranelli <renanranelli@gmail.com>
2016-04-21 15:49:41 -0300

    Use company yasnippet when inserting snippets

--- lisp/init-yas.el
+++ lisp/init-yas.el
@@ -20,7 +20,7 @@
 
 (define-key yas-minor-mode-map (kbd "C-c y n") 'yas-new-snippet)
 (define-key yas-minor-mode-map (kbd "C-c y f") 'yas-visit-snippet-file)
-(define-key yas-minor-mode-map (kbd "C-c y i") 'yas-insert-snippet)
+(define-key yas-minor-mode-map (kbd "C-c y i") 'company-yasnippet)
 
 ;; -- hooks --
 (add-hook 'prog-mode-hook
