724b836886387bd25d445ba7e986f78bc61caa9c
renan-ranelli <renanranelli@gmail.com>
2016-04-22 19:30:55 -0300

    More csharp goodies

--- lisp/init-csharp.el
+++ lisp/init-csharp.el
@@ -1,14 +1,29 @@
-;;; init-company.el -- Trolls the world by setting up a C# development environment.
-;;; Commentary: Omfg, this is so frikin nice
+;;; init-csharp.el -- Troll the world doing C# development with Emacs.
+;;; Commentary:
+;;;   Omfg, this is so frikin nice
 ;;; Code:
 (require 'csharp-mode)
 
-(setq omnisharp-server-executable-path "/home/renan/code/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe")
+(setq omnisharp-server-executable-path "/home/renan/code/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe"
+      omnisharp-auto-complete-popup-help-delay 2)
 
 (rr/define-bindings csharp-mode-map
-                    '(("M-/" . omnisharp-auto-complete)
-                      ("M-." . omnisharp-go-to-definition)
-                      ("C-4 M-." . omnisharp-go-to-definition-other-window)))
+                    '(("M-/"     . omnisharp-auto-complete)
+                      ("M-."     . omnisharp-go-to-definition)
+                      ("C-4 M-." . omnisharp-go-to-definition-other-window)
+                      ("C-c i"   . omnisharp-code-format)
+                      ("C-c C-c" . omnisharp-fix-code-issue-at-point)
+                      ("C-c x"   . omnisharp-run-code-action-refactoring)))
+
+(defun rr/omnisharp-rename-file ()
+  "Rename current file and fix the file path in .csproj file."
+  (interactive)
+  (let ((new-file-name (read-file-name "new name: ")))
+    (omnisharp-remove-from-project-current-file)
+    (dired-rename-file (buffer-file-name)
+                       new-file-name
+                       nil)
+    (omnisharp-add-to-solution-current-file)))
 
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-omnisharp))
