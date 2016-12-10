79ec13f09b6b9e3def6e70fea18a240c5befca86
Milhouse <renanranelli@gmail.com>
2016-12-10 18:53:07 -0200

    Haskell magic not working anymore

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -9,7 +9,7 @@
 (add-hook 'haskell-mode-hook 'inf-haskell-mode)
 
 ;; -- haskell font lock --
-(add-hook 'haskell-mode-hook 'turn-on-haskell-font-lock)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-font-lock)
 (remove-hook 'haskell-mode-hook 'pretty-symbols-mode)
 
 ;; removing this because of the conflict with flycheck
@@ -18,24 +18,24 @@
 ;; 			       (make-local-variable 'flycheck-display-errors-delay)
 ;; 			       (setq flycheck-display-errors-delay 5)))
 
-(defvar rr/haskell-font-lock-extra-symbols
-  '(("<alpha>" . #X03B1)
-    ("<beta>" . #X03B2)
-    ("<gamma>" . #X03B3)
-    ("<delta>" . #X03B4)
-    (".." . #X2026)
-    ("`elem`" . #X2208)
-    ("elem" . #X2208)
-    ("^" . #X2191)))
+;; (defvar rr/haskell-font-lock-extra-symbols
+;;   '(("<alpha>" . #X03B1)
+;;     ("<beta>" . #X03B2)
+;;     ("<gamma>" . #X03B3)
+;;     ("<delta>" . #X03B4)
+;;     (".." . #X2026)
+;;     ("`elem`" . #X2208)
+;;     ("elem" . #X2208)
+;;     ("^" . #X2191)))
 
-(eval-after-load 'haskell-font-lock
-  '(progn
-     (remove-alist 'haskell-font-lock-symbols-alist "()")
-     (mapcar
-      (lambda (entry) (add-to-list 'haskell-font-lock-symbols-alist entry))
-      rr/haskell-font-lock-extra-symbols)
-     (setq haskell-font-lock-keywords
-	   (haskell-font-lock-keywords-create nil))))
+;; (eval-after-load 'haskell-font-lock
+;;   '(progn
+;;      ;; (remove-alist 'haskell-font-lock-symbols-alist "()")
+;;      (mapcar
+;;       (lambda (entry) (add-to-list 'haskell-font-lock-symbols-alist entry))
+;;       rr/haskell-font-lock-extra-symbols)
+;;      (setq haskell-font-lock-keywords
+;; 	   (haskell-font-lock-keywords-create nil))))
 
 (provide 'init-haskell)
 ;;; init.el ends here
