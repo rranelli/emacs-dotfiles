19c3f9dad4dc7b8a59bcc97c7abafdcd3b2e847c
Milhouse <renanranelli@gmail.com>
2017-09-25 19:09:38 -0300

    more python stuff

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -3,11 +3,28 @@
 ;;; Code:
 
 ;; run this command to install required dependencies:
-;; `pip install jedi flake8 importmagic autopep8`'
-(when (require 'elpy nil t)
-  (elpy-enable))
+;; `sudo pip3 install jedi flake8 importmagic autopep8 ipython rope`'
+(require 'elpy)
 
 (setq elpy-rpc-backend "jedi")
+(elpy-enable)
+
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+(setq py-python-command-args ; use the wx backend, for both mayavi and matplotlib
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+(setq py-shell-switch-buffers-on-execute-p t) ; switch to the interpreter after executing code
+(setq py-switch-buffers-on-execute-p t)
+(setq py-split-windows-on-execute-p nil) ; don't split windows
+(setq py-smart-indentation t) ; try to automagically figure out indentation
+
+(define-key python-mode-map (kbd "C-c i") 'elpy-autopep8-fix-code)
+(define-key python-mode-map (kbd "C-c C-d") 'elpy-doc)
+
+(add-hook 'elpy-mode-hook (lambda ()
+                            (flymake-mode -1)
+                            (flycheck-mode 1)))
 
 (provide 'init-python)
 ;;; init-python.el ends here
--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -67,10 +67,7 @@ If ARG is present, open a new eshell regardless."
   (>= (line-number-at-pos (point))
       (line-number-at-pos (or (save-excursion
                                 (goto-char (point-max))
-                                (re-search-backward "$ " nil t))
-                              (save-excursion
-                                (goto-char (point-max))
-                                (re-search-backward "iex\([0-9]+\)>" nil t))
+                                (re-search-backward "\\($ \\|iex\([0-9]+\)>\\|>>>\\)" nil t))
                               0))))
 
 (defmacro rr/mimiterm-key (binding alternative-f)
