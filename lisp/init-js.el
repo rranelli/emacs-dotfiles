036ccad909cc83f3dfe6e83fe98ab262d2c879da
Milhouse <renanranelli@gmail.com>
2017-10-06 15:01:27 -0300

    Do not warn on trailling comma

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -6,6 +6,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
 
+(setq js2-strict-trailing-comma-warning nil)
+
 (add-hook 'js2-mode-hook 'indium-interaction-mode)
 
 (rr/expose-default-bindings js2-mode-map)
