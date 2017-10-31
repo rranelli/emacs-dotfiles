c598466adf27bdd5c88c84f8844f615767984434
Milhouse <renanranelli@gmail.com>
2017-10-31 10:18:26 -0200

    Try magit-gh-pulls once again

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -2,6 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 (require 'magit)
+(require 'magit-gh-pulls)
+(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
 
 (defun rr/magit-current-timemachine-review ()
   (interactive)
