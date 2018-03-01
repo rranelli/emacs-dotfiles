09e5501cefcf57e41cc0900f82cb4b55727bf2e4
Milhouse <renanranelli@gmail.com>
2018-03-01 15:58:57 -0300

    config elfeed
    Signed-off-by: Milhouse <renanranelli@gmail.com>

--- init.el
+++ init.el
@@ -42,6 +42,7 @@
 (use-package init-org :ensure nil)
 (use-package init-appearance :ensure nil)
 (use-package init-registers :ensure nil)
+(use-package init-feed :ensure nil)
 ;; init-haskell
 ;; init-clojure
 ;; init-csharp
---
+++ lisp/init-feed.el
@@ -0,0 +1,5 @@
+(use-package elfeed
+  :custom
+  (elfeed-feeds '("https://bravenewgeek.com/feed/")))
+
+(provide 'init-feed)
