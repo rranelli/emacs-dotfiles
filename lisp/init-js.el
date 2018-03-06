0c49cd4021d880a9f9da85658d580efceb63e8ee
Milhouse <renanranelli@gmail.com>
2018-03-06 18:49:06 -0300

    (use-package init-js)
    Signed-off-by: Milhouse <renanranelli@gmail.com>

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,20 +1,23 @@
 ;;; init-js.el --- Configures js editing
 ;;; Commentary:
 ;;; Code:
-(require 'js2-mode)
-(require 'indium)
+(use-package js2-mode
+  :mode "\\.js"
+  :custom
+  (js2-strict-trailing-comma-warning nil)
+
+  :config
+  (rr/expose-default-bindings js2-mode-map))
+
+(use-package indium
+  :hook
+  (js2-mode . indium-interaction-mode))
+
 ;; run `npm install -g tern' and add `.tern-project' to your project
 ;; into .tern-project:  {"plugins": {"node": {}, "es_modules": {}}}
-(require 'tern)
-
-(add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
-
-(setq js2-strict-trailing-comma-warning nil)
-
-(add-hook 'js2-mode-hook 'indium-interaction-mode)
-(add-hook 'js2-mode-hook 'tern-mode)
-
-(rr/expose-default-bindings js2-mode-map)
+(use-package tern
+  :hook
+  (js2-mode . tern-mode))
 
 (provide 'init-js)
 ;;; init-js.el ends here
