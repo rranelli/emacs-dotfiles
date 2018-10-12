ed68f03c0b805d17a08cb5211bdeb3fce016a93d
Milhouse <renanranelli@gmail.com>
2018-10-12 16:07:40 -0300

    yadda
    Signed-off-by: Milhouse <renanranelli@gmail.com>

--- lisp/init-restclient.el
+++ lisp/init-restclient.el
@@ -7,7 +7,6 @@
   :config
   (defun rr/pairs-to-query (pairs)
     (s-replace "=%3A" "=:" (url-build-query-string (-map (lambda (pair) (list (car pair) (cdr pair))) pairs) nil t)))
-
   (defun rr/restclient-scratch ()
     "Create a new restclient scratch buffer."
     (interactive)
